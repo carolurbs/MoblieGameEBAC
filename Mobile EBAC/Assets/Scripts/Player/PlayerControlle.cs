e5c3e4ac1516946b79285052ecd2481cbd3d4597
CarolUrbs <caroline.urbainski@edu.univali.br>
2022-12-22 15:32:24 -0300

    Obstáculos

--- Mobile EBAC/Assets/Scripts/Player/PlayerControlle.cs
+++ Mobile EBAC/Assets/Scripts/Player/PlayerControlle.cs
@@ -5,15 +5,33 @@ using UnityEngine;
 public class PlayerControlle : MonoBehaviour
 {
     public float speed = 1f; 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header ("LERP")]
+    public Transform target;
+    public float lerpSpeed = 1f;
+    public string tagToCheckEnemy = "Enemy";
 
-    // Update is called once per frame
+
+    private Vector3 _pos;
+    private bool _canRun;
+
+    public void Start()
+    {
+        _canRun = true;
+    }
     void Update()
     {
+        if (!_canRun) return;
+        _pos =target.position;
+        _pos.y= transform.position.y;
+        _pos.z= transform.position.z;
+        transform.position = Vector3.Lerp(transform.position, _pos, lerpSpeed * Time.deltaTime);
         transform.Translate(transform.forward*speed*Time.deltaTime);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == tagToCheckEnemy)
+        {
+            _canRun = false;
+        }
     }
 }
